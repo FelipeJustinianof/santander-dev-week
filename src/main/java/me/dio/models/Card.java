998d0f5fb7f897a7c54d6b55926ee800f2f7a9e7
Felp117 <felipe.8129@aluno.pr.senac.br>
2025-07-04 01:59:25 -0300

    Hotfix "precision" e "scale"

--- src/main/java/me/dio/models/Card.java
+++ src/main/java/me/dio/models/Card.java
@@ -12,7 +12,7 @@ public class Card {
     private Long id;
     @Column(unique = true)
     private String number;
-    @Column(name = "available", scale = 13, precision = 2)
+    @Column(name = "available", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
